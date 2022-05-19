652d1a7a41b62389c6e683b555a4ead6d5f265a6
Julien Sobel <sobeljulien@gmail.com>
2022-05-19 09:54:08 +0200

    conflits reglés, merci Davy ! ;)

--- front-page.php
+++ front-page.php
@@ -6,8 +6,7 @@ if (have_posts()) :
 ?>
         <div class="container">
             <h2><?php the_title() ?></h2>
-        <div><?php the_content() ?></div>
-<<<<<<< HEAD
+            <div><?php the_content() ?></div>
             <div class="row">
                 <div class="col">
                     <img class="img-fluid" src="<?php the_field('image_a_gauche') ?>" alt="">
@@ -15,15 +14,6 @@ if (have_posts()) :
                 <div class="col">
                     <p class="text-left"><?php the_field('texte_a_droite') ?></p>
                 </div>
-=======
-        <span class="pills"><?php the_category() ?></span>
-        <div class="row">
-            <div class="col">
-                <img class="img-fluid" src="<?php the_field("image_gauche") ?>" >
-            </div>
-            <div class="col">
-                <p><?php the_field("text_droite") ?></p>
->>>>>>> parent of e583da5 (commit du Mercredi)
             </div>
         </div>
 <?php endwhile;
--- functions.php
+++ functions.php
@@ -1,15 +1,9 @@
 <?php
-<<<<<<< HEAD
 function montheme_setup()
 {
-=======
-
-function projetJulienZDavy_Setup(){
->>>>>>> parent of e583da5 (commit du Mercredi)
     add_theme_support('post-thumbnails');
 }
 
-<<<<<<< HEAD
 function montheme_enqueue_styles()
 {
     wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css');
@@ -74,33 +68,3 @@ add_action('init', 'montheme_taxonomy');
 add_action('wp_enqueue_scripts', 'montheme_enqueue_styles');
 add_filter('nav_menu_css_class', 'montheme_menu_class_css');
 add_filter('nav_menu_link_attributes', 'montheme_menu_link_css');
-=======
-function mon_theme_scripts(){
-    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
-    wp_enqueue_style( 'style', get_stylesheet_uri() );
-    wp_enqueue_script('bootstrap','https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',[],false,true);
-}
-
-function mon_theme_custom_types(){
-    register_post_type('boardgame',[
-        'labels' => [
-            'name' => 'boardgames',
-            'singular_name' => 'boardgame'
-        ],
-            'public'      => true,
-            'menu_position' => 3,
-            'menu_icon'=>'dashicons-buddicons-activity',
-            'supports' => ['title', 'editor', 'thumbnail'],
-            'has_archive' => true,
-            'show_in_rest' => true
-
-    ]);
-}
-
-add_action('after_setup_theme', 'projetJulienZDavy_Setup');
-add_action('init', 'mon_theme_custom_types');
-add_action('wp_enqueue_scripts', 'projetJulienZDavy_Setup');
-
-
-
->>>>>>> parent of e583da5 (commit du Mercredi)
