a68d36320c80ec5fddc5fedd98204044d5bce361
davydebut <spanudavy@gmail.com>
2022-05-20 14:41:37 +0200

    Compréhension cours sur wordpress

---
+++ functions.php
@@ -0,0 +1,70 @@
+<?php
+
+function montheme_supports()
+{
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('menus');
+    register_nav_menu('header', 'En tête du menu');
+    register_nav_menu('footer', 'Pied de page');
+}
+
+function montheme_register_assests()
+{
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css');
+    wp_enqueue_style('style', get_stylesheet_uri());
+    wp_enqueue_script('script', get_template_directory_uri());
+    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js', [], false, true);
+}
+
+function montheme_title_separator()
+{
+    return '|';
+}
+
+function montheme_menu_class($classes)
+{
+    $classes[] = 'nav-item';
+    return $classes;
+}
+
+function montheme_menu_link_class($attrs)
+{
+    $attrs['class'] = 'nav-link';
+    return $attrs;
+}
+
+function montheme_document_title_parts($title)
+{
+    unset($title['tagline']);
+    return $title;
+}
+
+function montheme_pagination()
+{
+    $pages = paginate_links(['type' => 'array']);
+    if ($pages == null) {
+        return;
+    }
+    echo '<nav aria-label="Pagination class="my-4">';
+    echo '<ul class="pagination">';
+    foreach ($pages as $page) {
+        $active = strpos($page, 'current') !== false;
+        $class = 'page-item';
+        if ($active) {
+            $class .= ' active';
+        }
+        echo '<li class="' . $class . '">';
+        echo str_replace('page-numbers', 'page-link', $page);
+        echo '</li>';
+    }
+    echo '</ul>';
+    echo '</nav>';
+}
+
+add_action('after_setup_theme', 'montheme_supports');
+add_action('wp_enqueue_scripts', 'montheme_register_assests');
+add_filter('document_title_separator', 'montheme_title_separator');
+add_filter('document_title_parts', 'montheme_document_title_parts');
+add_filter('nav_menu_css_class', 'montheme_menu_class');
+add_filter('nav_menu_link_attributes', 'montheme_menu_link_class');
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+<?php if (have_posts()) : ?>
+    <div class="row">
+
+        <?php while (have_posts()) : the_post(); ?>
+            <div class="col-sm-4">
+                <div class="card">
+                    <a href="<?php the_permalink() ?>"><?php the_post_thumbnail('small', ['class' => 'card-img-top', 'alt' => '', 'style' => 'height: auto;']) ?></a>
+                    <div class="card-body">
+                        <h5 class="card-title"><?php the_title() ?></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php the_category() ?></h6>
+                        <p class="card-text"><?php the_excerpt() ?></p>
+                        <a href="<?php the_permalink() ?>" class="card-link">Voir plus</a>
+                    </div>
+                </div>
+            </div>
+        <?php endwhile ?>
+    </div>
+    <?php montheme_pagination() ?>
+    <?php echo paginate_links(); ?>
+<?php else : ?>
+    <h1>Pas d'articles</h1>
+<?php endif; ?>
+
+<?php get_footer(); ?>
